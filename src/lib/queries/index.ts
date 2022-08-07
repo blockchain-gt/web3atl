bf6e844260fdb056a552b3a18f11360e5446be97
Patrick Hultquist <patrick@monogram.io>
2022-08-07 01:23:45 -0400

    format query

--- src/lib/queries/index.ts
+++ src/lib/queries/index.ts
@@ -1,67 +1,65 @@
-const homePageQuery = `
-query {
-	page(where: {
-    slug: "index"
-  }) {
-    title
-    components {
-      __typename
-    	... on HeroSection {
-        title
-        description {
-          html
-        }
-        cta
-      }
-      ... on SpeakersSection {
-       	title
-        agendaSpeakers {
-          person {
-            name
-            company
-            title
-          }
-          tags
-        }
-      }
-      ... on TicketsSection {
-        title
-        description {
-          html
-        }
-      }
-      ... on SponsorsSection {
-        title
-        sponsors {
-          tier
-          image {
-            url
-          }
-          link 
-        }
-      }
-      ... on FaqSection {
-        title
-        faqs {
-          question
-          answer {
-            html
-          }
-        }
-      }
-    }
-  }
-  
-  # Get all ticket phases
-  ticketPhases(orderBy: endDate_ASC) {
-    ...TicketPhase
-  }
-}
+const homePageQuery = /* GraphQL */ `
+	query {
+		page(where: { slug: "index" }) {
+			title
+			components {
+				__typename
+				... on HeroSection {
+					title
+					description {
+						html
+					}
+					cta
+				}
+				... on SpeakersSection {
+					title
+					agendaSpeakers {
+						person {
+							name
+							company
+							title
+						}
+						tags
+					}
+				}
+				... on TicketsSection {
+					title
+					description {
+						html
+					}
+				}
+				... on SponsorsSection {
+					title
+					sponsors {
+						tier
+						image {
+							url
+						}
+						link
+					}
+				}
+				... on FaqSection {
+					title
+					faqs {
+						question
+						answer {
+							html
+						}
+					}
+				}
+			}
+		}
 
-fragment TicketPhase on TicketPhase {
-  price
-  endDate
-}
+		# Get all ticket phases
+		ticketPhases(orderBy: endDate_ASC) {
+			...TicketPhase
+		}
+	}
+
+	fragment TicketPhase on TicketPhase {
+		price
+		endDate
+	}
 `
 
 export default homePageQuery
