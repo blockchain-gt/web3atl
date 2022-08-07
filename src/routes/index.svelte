a1f3d7581e0337858e6705371dae176296f055a3
Patrick Hultquist <patrick@monogram.io>
2022-08-07 01:33:53 -0400

    json on page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,8 @@
 <script lang="ts" context="module">
 	import homePageQuery from '$lib/queries'
+	import type { Page, TicketPhase } from '$lib/types/models'
 
-	export async function load({ fetch, params }) {
+	export async function load({ params }) {
 		const res = await fetch(
 			'https://api-us-east-1.hygraph.com/v2/cl6isf8724r4g01uh7l9w44u3/master',
 			{
@@ -14,18 +15,29 @@
 
 		return {
 			status: 200,
-			props: json.data
+			props: {
+				page: json.data.page,
+				ticketPhases: json.data.ticketPhases
+			}
 		}
 	}
 </script>
 
 <script lang="ts">
-	export let data
+	export let page: Page
+	export let ticketPhases: TicketPhase[]
 </script>
 
 <svelte:head>
-	{}
+	{page.title}
 </svelte:head>
 
+<pre>
+{JSON.stringify(page, null, 2)}
+</pre>
+
+<pre>
+{JSON.stringify(ticketPhases, null, 2)}
+</pre>
 <h1>Web3 ATL</h1>
-<p></p>
+<p />
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-vercel'
 import preprocess from 'svelte-preprocess'
-import { resolve } from 'path'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
