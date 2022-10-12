243f3d9ffdd6c292c9f9a5d4e9cfafaf9d17b8ed
Patrick Hultquist <patrick@monogram.io>
2022-10-11 21:00:34 -0400

    add buttons to schedule page

--- site/src/components/sections/Schedule.svelte
+++ site/src/components/sections/Schedule.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
 	import ScheduleItem from '$components/ScheduleItem.svelte';
-	import type { OrganizersSection, ScheduleSection } from '$lib/types/components';
+	import type { ScheduleSection } from '$lib/types/components';
 
 	export let section: ScheduleSection;
 
-	const byDay = section.agendaItems.reduce((acc, item) => {
+	const filters = [
+		{ id: 'all', name: 'All' },
+		{ id: 'hackathon', name: 'Hackathon' },
+		{ id: 'satelliteEvent', name: 'Satellite Event' },
+		{ id: 'speakerSeries', name: 'Speaker Series' }
+	];
+
+	let selectedEventTypeId = filters[0].id;
+
+	$: byDay = section.agendaItems.reduce((acc, item) => {
+		if (item.eventType !== selectedEventTypeId && selectedEventTypeId !== 'all') {
+			return acc;
+		}
 		const time = new Date(item.startTime);
 
 		const day = time.toLocaleDateString('en-US', {
@@ -21,6 +33,21 @@
 </script>
 
 <div class="px-4">
+	<section class="max-w-screen-lg">
+		<div class="flex gap-x-4">
+			{#each filters as filter}
+				{@const isSelected = selectedEventTypeId === filter.id}
+				<button
+					on:click={() => {
+						selectedEventTypeId = filter.id;
+					}}
+					class="rounded-md border border-black/10 min-w-[100px] {isSelected
+						? 'bg-pink text-white'
+						: 'bg-white text-black'} p-3">{filter.name}</button
+				>
+			{/each}
+		</div>
+	</section>
 	<section
 		class="space-y-0 max-w-screen-lg pb-10 backdrop-blur-lg bg-gray-400/10 border border-black/10 mb-10 rounded-2xl"
 	>
