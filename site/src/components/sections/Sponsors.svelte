01bcf5512f172722a7196f4b5f51b4eed2465fdf
Patrick Hultquist <patrick@monogram.io>
2022-10-12 01:19:39 -0400

    addr fixes

--- site/src/components/sections/Sponsors.svelte
+++ site/src/components/sections/Sponsors.svelte
@@ -4,7 +4,7 @@
 	export let section: SponsorsSection;
 
 	const colors: Record<string, string> = {
-		platinum: 'bg-gradient-to-r from-gray-400 to-gray-600 font-extrabold drop-shadow-xl',
+		platinum: 'bg-gradient-to-r from-gray-400 to-blue-300 font-extrabold drop-shadow-xl',
 		gold: 'bg-gradient-to-r from-amber-500 to-amber-600',
 		silver: 'bg-gradient-to-r from-gray-400 to-gray-600',
 		bronze: 'bg-gradient-to-r from-amber-600 to-amber-800',
@@ -23,7 +23,7 @@
 					href={organizer.link}
 					class="h-56 w-64 md:w-80 max-w-full flex flex-col items-center justify-between gap-y-2 text-center"
 				>
-					<img src={organizer.image?.url} class="rounded-md" alt={organizer.link} />
+					<img src={organizer.image?.url} class="rounded-md my-auto" alt={organizer.link} />
 					<p
 						class="font-[adventure] uppercase text-2xl bg-clip-text text-transparent {colors[
 							organizer.tier.toLowerCase()
