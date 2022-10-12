ee43bedf39375bd395f46535cf2f56c9a6dad1fc
Patrick Hultquist <patrick@monogram.io>
2022-10-11 23:14:18 -0400

    sponsors section

--- site/src/components/sections/Sponsors.svelte
+++ site/src/components/sections/Sponsors.svelte
@@ -1,48 +1,39 @@
 <script lang="ts">
 	import type { SponsorsSection } from '$lib/types/components';
-	import type { Sponsor } from '$lib/types/models';
 
 	export let section: SponsorsSection;
 
-	const groupedSections = section.sponsors.reduce<Record<string, Sponsor[]>>((acc, section) => {
-		if (!acc[section.tier]) {
-			acc[section.tier] = [];
-		}
-
-		acc[section.tier].push(section);
-
-		return acc;
-	}, {});
-
-	// Group groupedSection by section.tierOrder
-	const sortedGroupSections = Object.entries(groupedSections)
-		.sort(([tier1], [tier2]) => {
-			const tier1Order = section.tierOrder.indexOf(tier1);
-			const tier2Order = section.tierOrder.indexOf(tier2);
-
-			return tier1Order - tier2Order;
-		})
-		.map(([tier, sponsors]) => ({ tier, sponsors }));
+	const colors: Record<string, string> = {
+		platinum: 'bg-gradient-to-r from-gray-400 to-gray-600 font-extrabold drop-shadow-xl',
+		gold: 'bg-gradient-to-r from-amber-500 to-amber-600',
+		silver: 'bg-gradient-to-r from-gray-400 to-gray-600',
+		bronze: 'bg-gradient-to-r from-amber-600 to-amber-800',
+		local: 'bg-gradient-to-r from-blue-400 to-blue-400'
+	};
 </script>
 
-<!-- <pre>
-{JSON.stringify(section, null, 2)}
-</pre> -->
-
-<section>
-	<h2>
+<div class="py-16 px-20 bg-gray-100 space-y-8">
+	<h2 class="text-4xl md:text-5xl">
 		{section.title}
 	</h2>
-	{#each sortedGroupSections as tierGroup}
-		<h3 class="mb-4">
-			{tierGroup.tier}
-		</h3>
-		<div class="flex flex-wrap">
-			{#each tierGroup.sponsors as sponsor}
-				<a href={sponsor.link} class="overflow-hidden">
-					<img src={sponsor.image.url} class="w-80" alt="{sponsor.tier} Sponsor {sponsor.link}" />
+	<div>
+		<div class="grid gap-x-5 gap-y-9 grid-cols-1 md:grid-cols-4 items-center justify-around">
+			{#each section.sponsors as organizer}
+				<a
+					href={organizer.link}
+					class="h-56 w-64 md:w-80 max-w-full flex flex-col items-center justify-between gap-y-2 text-center"
+				>
+					<img src={organizer.image?.url} class="rounded-md" alt={organizer.link} />
+					<p
+						class="font-[adventure] uppercase text-2xl bg-clip-text text-transparent {colors[
+							organizer.tier.toLowerCase()
+						]}"
+					>
+						<!-- <p class="font-[adventure] uppercase text-2xl {}"> -->
+						{organizer.tier}
+					</p>
 				</a>
 			{/each}
 		</div>
-	{/each}
-</section>
+	</div>
+</div>
