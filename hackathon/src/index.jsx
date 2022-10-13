c57219934bb229bd88249fad3d7b60412c67d200
Patrick Hultquist <patrick@monogram.io>
2022-10-13 01:20:35 -0400

    fuck

--- hackathon/src/index.jsx
+++ hackathon/src/index.jsx
@@ -7,6 +7,13 @@ import MetaballSwarm from './MetaballSwarm/MetaballSwarm'
 
 import './styles.css'
 
+const faqs = [
+  {
+    question: 'How do I play?',
+    answer: 'Use the arrow keys to move the ball around. Try to avoid the obstacles and collect the coins.'
+  }
+]
+
 const useHasMounted = () => {
   const [hasMounted, setHasMounted] = useState(false)
   useEffect(() => {
@@ -21,8 +28,8 @@ function App() {
   const urls = [
     'https://media.graphassets.com/RmZ06tMSj6st04ov8lkh',
     'https://media.graphassets.com/U7IY50hxQyRtmm5CjURM',
-    'https://media.graphassets.com/rTq3hpFTGqzVkXvT2gQ7',
-    'https://media.graphassets.com/Nw6xvqTARD6EGkh38yP2'
+    'https://media.graphassets.com/XL8aVyXoQ9iU1LZYw09d',
+    'https://media.graphassets.com/q5n0pT5Q1Oqv1LzdA1Gp'
   ]
   const hasMounted = useHasMounted()
 
@@ -39,10 +46,10 @@ function App() {
       </div>
       {/* First fold */}
       <section className="text-white z-20 flex max-w-screen-2xl mx-auto justify-end items-center min-h-screen drop-shadow">
-        <div className="max-w-[600px]">
+        <div className="max-w-[min(600px,100vw)] px-4">
           <h3 className={`text-3xl font-extrabold  transition-all duration-1000 ease-out ${hasMounted ? 'opacity-75' : 'opacity-0 translate-y-4'}`}>Web3ATL</h3>
           <h1
-            className={`text-8xl font-black drop-shadow-2xl transition-all duration-1000 delay-500 ease-out font-mono ${
+            className={`text-5xl md:text-8xl font-black drop-shadow-2xl transition-all duration-1000 delay-500 ease-out font-mono ${
               hasMounted ? '' : 'opacity-0 translate-y-4'
             }`}>
             Hackathon.
@@ -53,7 +60,7 @@ function App() {
               voluptates doloremque corporis rerum voluptatibus adipisci ut placeat facilis amet.
             </p>
             <p className="uppercase text-white font-bold mb-8 ">Oct 26 - Nov 6</p>
-            <div className={`grid grid-cols-2 gap-4`}>
+            <div className={`grid md:grid-cols-2 gap-4 text-center`}>
               <a href="https://forms.gle/YN6NSAbe2CyuzADc7" className="cta-button">
                 Apply
               </a>
@@ -65,7 +72,7 @@ function App() {
         </div>
       </section>
       {/* Sponsors */}
-      <div className="relative h-0 flex justify-center">
+      <div className="hidden md:relative h-0 flex justify-center">
         <div className="absolute z-40 text-white max-w-screen-lg mx-auto glass -translate-y-1/2 grid grid-cols-1 md:grid-cols-4 items-center gap-12">
           {urls.map((url, i) => (
             <img src={url} alt="" />
@@ -73,9 +80,9 @@ function App() {
         </div>
       </div>
       {/* Track 1 */}
-      <section className="backdrop-blur-2xl border-y border-white/20 backdrop-brightness-50 section-thing">
-        <img src={urls[0]} alt="" />
-        <div className="border-card bg-black/50 w-[500px]">
+      <section className="blur-and-dark section-thing">
+        <img src={urls[2]} alt="" />
+        <div className="border-card bg-black/50 w-[min(500px,90vw)]">
           <h2 className="text-3xl font-bold">Solidity Track</h2>
           <p className="opacity-80 my-4">
             On Saturday November 5th, hackers will compete in a 1 hour solidity competition where they try to make the contract evaluate to true. Participants
@@ -89,7 +96,7 @@ function App() {
       </section>
       {/* Track 2 */}
       <section className="section-thing">
-        <div className="glass w-[500px]">
+        <div className="glass w-[min(500px,90vw)]">
           <h2 className="text-3xl font-bold">Data Track</h2>
           <p className="opacity-80 my-4">
             Beginning October 26th, hackers will compete to build an ETH price prediction model using the ocean protocol SDK. Winner will be decided by taking
@@ -100,11 +107,12 @@ function App() {
             Problem Details
           </a>
         </div>
+        <img src={urls[1]} alt="" className="drop-shadow" />
       </section>
       {/* Track 3 */}
-      <section className="backdrop-blur-2xl border-y border-white/20 section-thing">
+      <section className="blur-and-dark section-thing">
         <img src={urls[0]} alt="" />
-        <div className="border-card bg-black/50 w-[500px]">
+        <div className="border-card bg-black/50 w-[min(500px,90vw)]">
           <h2 className="text-3xl font-bold">Security Track</h2>
           <p className="opacity-80 my-4">
             Hackers will leverage Forta&apos;s SDK to build detection bots. Detection bots digest blockchain data to detect some condition and can make network
@@ -115,8 +123,53 @@ function App() {
           </a>
         </div>
       </section>
-      <section className="w-full backdrop-blur-lg bg-black/50 p-10 border-t border-white/20 text-white">
-        <div className="max-w-screen-xl mx-auto">this is a test</div>
+      {/* Track 4 */}
+      <section className="section-thing">
+        <div className="glass w-[min(500px,90vw)]">
+          <h2 className="text-3xl font-bold">Data Track</h2>
+          <p className="opacity-80 my-4">
+            Beginning October 26th, hackers will compete to build an ETH price prediction model using the ocean protocol SDK. Winner will be decided by taking
+            the NMSE of the predicted price versus actual price over a 24 1-hour intervals.
+          </p>
+          <DifficultySlider num={2} title="Test" />
+          <a href="https://docs.google.com/document/d/17xHAAFSGQvqFVjnnMepil4Jwwq2iCHX2RQSQ3SnozX0/edit?usp=sharing" className="button">
+            Problem Details
+          </a>
+        </div>
+        <img src={urls[1]} alt="" className="drop-shadow" />
+      </section>
+      <section className="section-thing blur-and-dark !h-auto py-20">
+        <div className="w-full space-y-10">
+          <div className="flex flex-col md:flex-row w-full justify-around px-8 gap-6 min-w-[300px] mx-auto drop-shadow">
+            <a href="" className="button hover:text-white !bg-black/50">
+              Hack Pack
+            </a>
+            <a href="" className="button hover:text-white !bg-black/50">
+              View Schedule
+            </a>
+            <a href="" className="button hover:text-white !bg-black/50">
+              Apply
+            </a>
+            <a href="" className="button hover:text-white !bg-black/50">
+              Join Discord
+            </a>
+          </div>
+          <div className="px-20">
+            <h2 className="mb-4 mt-4 font-bold text-4xl">FAQs</h2>
+            <div className="faq">
+              {faqs.map((faq, i) => (
+                <>
+                  <h3 className="" key={faq.question}>
+                    {faq.question}
+                  </h3>
+                  <p key={faq.question} className="text-gray-300">
+                    {faq.answer}
+                  </p>
+                </>
+              ))}
+            </div>
+          </div>
+        </div>
       </section>
     </>
   )
